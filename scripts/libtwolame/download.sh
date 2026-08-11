9faec83be28a4c4939786c5d3d6afcab580c2a1b
cuicanrensheng <1693728915@qq.com>
2026-08-11 20:05:02 +0800

    Delete scripts directory

--- scripts/libtwolame/download.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source ${SCRIPTS_DIR}/common-functions.sh
-
-TWOLAME_VERSION=0.4.0
-
-downloadTarArchive \
-  "libtwolame" \
-  "https://downloads.videolan.org/pub/contrib/twolame/twolame-${TWOLAME_VERSION}.tar.gz"
--- scripts/mbedtls/android.cmake
+++
@@ -1,5 +0,0 @@
-include("$ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake")
-
-if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "i686")
-  set(CMAKE_C_FLAGS "-mpclmul -msse2 -maes")
-endif()
